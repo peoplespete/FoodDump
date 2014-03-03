2f72b2dc08629cfed0eaad5b04ec0aab6e196892
Pete Dunlap <petedunlap@gmail.com>
2014-03-02 18:48:53 -0600

    ratings files added

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,35 @@
+class RatingsController < ApplicationController
+
+  def new
+    @rating = Rating.new
+  end
+
+  def create
+    # go ahead and make the rating for all pending ingredients
+    rating = rating_params[:scale].to_i
+    if rating.to_s == rating_params[:scale]
+      # this verifies that the rating was really a number (ruby converts 'r'.to_i to 0)
+      @ingredients = Ingredient.where(pending: true)
+
+      @ingredients.each do |ingredient|
+        Rating.create(rating: rating, ingredient_id: ingredient.id)
+        ingredient.pending = false
+        ingredient.save
+      end
+
+      flash[:success] = "Rating of #{rating} Added!"
+    else
+      flash[:error] = "Sorry we couldn't add that rating, please try again with a different rating"
+    end
+
+    redirect_to new_rating_path
+  end
+
+
+  private
+
+    def rating_params
+      params.require(:rating).permit(:scale)
+    end
+
+end
